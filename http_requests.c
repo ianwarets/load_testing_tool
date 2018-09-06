abd78fad4ad7196af48383d79b055ecce70b4ac0
Окунев Иван <iokunev@neoflex.ru>
2018-09-06 10:57:55 +0300

    Добавлен метод получение информации о транзакции,

--- http_requests.c
+++ http_requests.c
@@ -5,6 +5,7 @@
 
 static CURLcode http_request(CURL *, char*, struct curl_slist *, response_data_struct *);
 static size_t save_response_data(void*, size_t, size_t, void *);
+static void check_getinfo_result(CURLcode, curl_off_t *);
 
 /*
 	Выполнение GET запроса.
@@ -20,6 +21,41 @@ CURLcode get_request(CURL * hCurl, char * url, struct curl_slist * headers, resp
 CURLcode post_request(CURL * hCurl, char * url, struct curl_slist * headers, response_data_struct * callback_data){
 	curl_easy_setopt(hCurl, CURLOPT_HTTPPOST, 1L);
 	return http_request(hCurl, url, headers, callback_data);
+}
+
+/*
+	Сбор статистики по транзакции
+*/
+transaction_tech_data collect_request_statistics(CURL * hCurl){
+	curl_off_t response_time;
+	curl_off_t network_delay;
+	curl_off_t uploaded_bytes;
+	curl_off_t received_bytes;
+	curl_off_t header_size;
+	curl_off_t upload_speed;
+	curl_off_t download_speed;
+
+	// Returns CURLE_OK if the option is supported, and CURLE_UNKNOWN_OPTION if not. 
+	check_getinfo_result(curl_easy_getinfo(hCurl, CURLINFO_TOTAL_TIME_T, &response_time),&response_time);
+	// Время установления соединения.
+	check_getinfo_result(curl_easy_getinfo(hCurl, CURLINFO_CONNECT_TIME_T, &network_delay),&network_delay);
+	check_getinfo_result(curl_easy_getinfo(hCurl, CURLINFO_REQUEST_SIZE, &uploaded_bytes),&uploaded_bytes);
+	// Объем полезных загруженных данных, не включает метаданные. 
+	check_getinfo_result(curl_easy_getinfo(hCurl, CURLINFO_SIZE_DOWNLOAD_T, &received_bytes),&received_bytes);
+	// Суммарный объем заголовков.
+	check_getinfo_result(curl_easy_getinfo(hCurl, CURLINFO_HEADER_SIZE, &header_size),&header_size);
+	check_getinfo_result(curl_easy_getinfo(hCurl, CURLINFO_SPEED_UPLOAD_T, &upload_speed),&upload_speed);
+	check_getinfo_result(curl_easy_getinfo(hCurl, CURLINFO_SPEED_DOWNLOAD_T, &download_speed),&download_speed);
+	
+	transaction_tech_data tech_data_struct = {
+		.response_time = response_time,
+		.netw_delay = network_delay,
+		.uploaded_bytes = uploaded_bytes,
+		.received_bytes = received_bytes + header_size,
+		.upload_speed = upload_speed,
+		.download_speed = download_speed
+	};
+	return tech_data_struct;
 }
 
 /*
@@ -47,12 +83,7 @@ static CURLcode http_request(CURL * request_handle, char * url, struct curl_slis
 	curl_easy_setopt(request_handle, CURLOPT_ACCEPT_ENCODING, "");
 
 	curl_easy_setopt(request_handle, CURLOPT_WRITEFUNCTION, save_response_data);
-	curl_easy_setopt(request_handle, CURLOPT_WRITEDATA, callback_data);
-	
-	// Включение автоматического перехода по redirect адресам
-	curl_easy_setopt(request_handle, CURLOPT_FOLLOWLOCATION, 1L);
-	// Автоматическое задание заголовка Referrer при переходе по Redirect
-	curl_easy_setopt(request_handle, CURLOPT_AUTOREFERER, 1L);
+	curl_easy_setopt(request_handle, CURLOPT_WRITEDATA, callback_data);	
 
 	// Задание заголовков запроса.
 	if(headers != NULL){
@@ -106,4 +137,14 @@ static size_t save_response_data(void* buffer, size_t size, size_t nmemb, void *
 	}
 	((response_data_struct*)userp)->size += result;
 	return result;
+}
+
+/*
+	Задает значение результата -1, если код = CURLE_UNKNOWN_OPTION
+*/
+static void check_getinfo_result(CURLcode code, curl_off_t * result){
+	if(code == CURLE_UNKNOWN_OPTION){
+		*result = -1;
+	}
+	return;
 }
--- http_requests.h
+++ http_requests.h
@@ -7,13 +7,21 @@ typedef struct {
 }response_data_struct;
 
 typedef struct {
-    void * response_time;
-    void * netw_delay;
-    long uploaded_bytes;
-    long received_bytes;
-    long upload_speed;
-    long download_speed;    
+    // Response time in microseconds.
+    curl_off_t  response_time;
+    // Network delay time in microseconds.
+    curl_off_t netw_delay;
+    // Amount of uploaded bytes.
+    curl_off_t uploaded_bytes;
+    // Amount of received bytes.
+    curl_off_t received_bytes;
+    // Upload speed in bytes/second
+    curl_off_t  upload_speed;
+    // Download speed in bytes/second
+    curl_off_t  download_speed;    
 }transaction_tech_data;
+
 
 CURLcode get_request(CURL *, char*, struct curl_slist *, response_data_struct *);
 CURLcode post_request(CURL *, char*, struct curl_slist *, response_data_struct *);
+transaction_tech_data collect_request_statistics(CURL *);
