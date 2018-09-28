7843b0d6709252b0c9c7db1ffc927c36a649284a
Окунев Иван <iokunev@neoflex.ru>
2018-09-28 10:14:45 +0300

    Добалвены цели созданиеястатических библиотек для написания  сценария

--- creator/http_requests.c
+++ creator/http_requests.c
@@ -1,24 +1,22 @@
 #include <curl/curl.h>
 #include <string.h>
 #include <time.h>
+#include <logger.h>
 #include "http_requests.h"
 
 
 static long http_request(char *, char*, struct curl_slist *, response_data_struct *);
 static size_t save_response_data(void*, size_t, size_t, void *);
 static void save_request_statistics();
-CURL * hCurl;
-zlog_category_t * zlog_programm_category;
-zlog_category_t * zlog_statistics;
+static CURL * hCurl;
+extern zlog_categories * loggers;
 
-int init_http_requests(zlog_category_t * programm_log, zlog_category_t * statistics){
+int init_http_requests(){
 	hCurl = curl_easy_init();
 	if(hCurl == NULL){
-		zlog_fatal(programm_log, "Failed to create CURL handler");
+		zlog_fatal(loggers->scenario, "Failed to create CURL handler");
 		return -1;
 	}
-	zlog_programm_category = programm_log;
-	zlog_statistics = statistics;
 	return 0;
 }
 
@@ -36,8 +34,8 @@ void http_request_cleanup(){
  */
 
 long get_request(char * name, char * url, struct curl_slist * headers, response_data_struct * response_data){		
-	zlog_info(zlog_programm_category, "get_request");
-	zlog_debug(zlog_programm_category, "URL:%s, curl_slist has value:%c, response_data_struct has value:%c", url, ((headers == NULL) ? 'n' : 'y'), ((response_data == NULL) ? 'n' : 'y'));
+	zlog_info(loggers->scenario, "get_request");
+	zlog_debug(loggers->scenario, "URL:%s, curl_slist has value:%c, response_data_struct has value:%c", url, ((headers == NULL) ? 'n' : 'y'), ((response_data == NULL) ? 'n' : 'y'));
 	curl_easy_setopt(hCurl, CURLOPT_HTTPGET, 1L);
 	return http_request(name, url, headers, response_data);	
 }
@@ -50,61 +48,71 @@ long get_request(char * name, char * url, struct curl_slist * headers, response_
  * 	@response_data - указатель на структуру предназначенную для сохранения полученного в результате запроса ответа. 
  */
 long post_request(char * name, char * url, struct curl_slist * headers, response_data_struct * response_data){
-	zlog_info(zlog_programm_category, "post_request");
-	zlog_debug(zlog_programm_category, "URL:%s, curl_slist has value:%c, response_data_struct has value:%c", url, (headers == NULL) ? 'n' : 'y', (response_data == NULL) ? 'n' : 'y');
+	zlog_info(loggers->scenario, "post_request");
+	zlog_debug(loggers->scenario, "URL:%s, curl_slist has value:%c, response_data_struct has value:%c", url, (headers == NULL) ? 'n' : 'y', (response_data == NULL) ? 'n' : 'y');
 	curl_easy_setopt(hCurl, CURLOPT_HTTPPOST, 1L);
 	return http_request(name, url, headers, response_data);
 }
 
 /**
- *	Сбор статистики по запросу.
+ *	Сбор и сохранение статистики по запросу.
  */
 static void save_request_statistics(char * name){
-	zlog_debug(zlog_programm_category, "hCurl has value:%c", (hCurl == NULL) ? 'n' : 'y');
+	zlog_debug(loggers->scenario, "hCurl has value:%c", (hCurl == NULL) ? 'n' : 'y');
 
 	curl_off_t response_time = -1;
-	zlog_info(zlog_programm_category, "Getting: CURLINFO_TOTAL_TIME_T");
+	curl_off_t network_delay = -1;
+	long uploaded_bytes = -1;
+	curl_off_t received_bytes = -1;
+	long header_size = -1;
+	curl_off_t upload_speed = -1;
+	curl_off_t download_speed = -1;
+	time_t * time = NULL;	
+	
+	zlog_info(loggers->scenario, "Getting: CURLINFO_TOTAL_TIME_T");
 	// Returns CURLE_OK if the option is supported, and CURLE_UNKNOWN_OPTION if not. 
 	curl_easy_getinfo(hCurl, CURLINFO_TOTAL_TIME_T, &response_time);
-	zlog_debug(zlog_programm_category, "CURLINFO_TOTAL_TIME_T : %lli", response_time);
 
-	curl_off_t network_delay = -1;
-	zlog_info(zlog_programm_category, "Getting: CURLINFO_CONNECT_TIME_T");
+	zlog_info(loggers->scenario, "Getting: CURLINFO_CONNECT_TIME_T");
 	// Время установления соединения.
 	curl_easy_getinfo(hCurl, CURLINFO_CONNECT_TIME_T, &network_delay);
-	zlog_debug(zlog_programm_category, "CURLINFO_CONNECT_TIME_T : %lli", network_delay);
 
-	long uploaded_bytes = -1;
-	zlog_info(zlog_programm_category, "Getting: CURLINFO_REQUEST_SIZE");
+	zlog_info(loggers->scenario, "Getting: CURLINFO_REQUEST_SIZE");
 	curl_easy_getinfo(hCurl, CURLINFO_REQUEST_SIZE, &uploaded_bytes);
-	zlog_debug(zlog_programm_category, "CURLINFO_REQUEST_SIZE : %li", uploaded_bytes);
 
-	curl_off_t received_bytes = -1;
-	zlog_info(zlog_programm_category, "Getting: CURLINFO_SIZE_DOWNLOAD_T");
+	zlog_info(loggers->scenario, "Getting: CURLINFO_SIZE_DOWNLOAD_T");
 	// Объем полезных загруженных данных, не включает метаданные. 
 	curl_easy_getinfo(hCurl, CURLINFO_SIZE_DOWNLOAD_T, &received_bytes);
-	zlog_debug(zlog_programm_category, "CURLINFO_SIZE_DOWNLOAD_T : %lli", received_bytes);
 
-	long header_size = -1;
-	zlog_info(zlog_programm_category, "Getting: CURLINFO_HEADER_SIZE");
+	zlog_info(loggers->scenario, "Getting: CURLINFO_HEADER_SIZE");
 	// Суммарный объем заголовков.
 	curl_easy_getinfo(hCurl, CURLINFO_HEADER_SIZE, &header_size);
-	zlog_debug(zlog_programm_category, "CURLINFO_HEADER_SIZE : %li", header_size);
 
-	curl_off_t upload_speed = -1;
-	zlog_info(zlog_programm_category, "Getting: CURLINFO_SPEED_UPLOAD_T");
+	zlog_info(loggers->scenario, "Getting: CURLINFO_SPEED_UPLOAD_T");
 	curl_easy_getinfo(hCurl, CURLINFO_SPEED_UPLOAD_T, &upload_speed);
-	zlog_debug(zlog_programm_category, "CURLINFO_SPEED_UPLOAD_T : %lli", upload_speed);
 	
-	curl_off_t download_speed = -1;
-	zlog_info(zlog_programm_category, "Getting: CURLINFO_SPEED_DOWNLOAD_T");
+	zlog_info(loggers->scenario, "Getting: CURLINFO_SPEED_DOWNLOAD_T");
 	curl_easy_getinfo(hCurl, CURLINFO_SPEED_DOWNLOAD_T, &download_speed);
-	zlog_debug(zlog_programm_category, "CURLINFO_SPEED_DOWNLOAD_T : %lli", download_speed);
 	
-	time_t * time = NULL;	
-	zlog_info(zlog_programm_category, "Getting: CURLINFO_PRIVATE");
+	zlog_info(loggers->scenario, "Getting: CURLINFO_PRIVATE");
 	curl_easy_getinfo(hCurl, CURLINFO_PRIVATE, &time);
-	zlog_debug(zlog_programm_category, "CURLINFO_PRIVATE : [time]:%li", *time);
+
+	zlog_debug(loggers->scenario, "\tCURLINFO_TOTAL_TIME_T : %lli\n\
+	CURLINFO_CONNECT_TIME_T : %lli\n\
+	CURLINFO_REQUEST_SIZE : %li\n\
+	CURLINFO_SIZE_DOWNLOAD_T : %lli\n\
+	CURLINFO_HEADER_SIZE : %li\n\
+	CURLINFO_SPEED_UPLOAD_T : %lli\n\
+	CURLINFO_SPEED_DOWNLOAD_T : %lli\n\
+	CURLINFO_PRIVATE : [time]:%li", 
+	response_time,
+	network_delay,
+	uploaded_bytes,
+	received_bytes,
+	header_size,
+	upload_speed,
+	download_speed,
+	time);
 
 	// return(request_tech_data){
 	// 	.time = time,
@@ -116,7 +124,7 @@ static void save_request_statistics(char * name){
 	// 	.download_speed = download_speed
 	// };
 	char * message_template = "%li,%s,%lli,%lli,%li,%lli,%lli,%lli";
-	zlog_info(zlog_statistics, 
+	zlog_info(loggers->statistics, 
 			message_template, 
 			*time,
 			name,
@@ -138,7 +146,7 @@ static void save_request_statistics(char * name){
  */
 static long http_request(char * name, char * url, struct curl_slist * headers, response_data_struct * response_data){
 	if(name == NULL){
-			zlog_error(zlog_programm_category, "Request name is not set!");
+			zlog_error(loggers->scenario, "Request name is not set!");
 			return -1;
 	}
 
@@ -148,47 +156,47 @@ static long http_request(char * name, char * url, struct curl_slist * headers, r
 		curl_easy_setopt(hCurl, CURLOPT_VERBOSE, 1L);
 	#endif
 
-	zlog_debug(zlog_programm_category, "CURLOPT_URL: %s", url);
+	zlog_debug(loggers->scenario, "CURLOPT_URL: %s", url);
 	// Set URL
 	curl_easy_setopt(hCurl, CURLOPT_URL, url);	
 
-	zlog_info(zlog_programm_category, "Enabling TLS.");
+	zlog_info(loggers->scenario, "Enabling TLS.");
 	curl_easy_setopt(hCurl, CURLOPT_USE_SSL, CURLUSESSL_TRY);	
 	
-	zlog_info(zlog_programm_category, "Setting certificate storage file name.");
+	zlog_info(loggers->scenario, "Setting certificate storage file name.");
 	// Задание пути к файлу сертификатов.
 	curl_easy_setopt(hCurl, CURLOPT_CAINFO, cert_file_name);
 	
-	zlog_info(zlog_programm_category, "Setting accept encoding to all types.");
+	zlog_info(loggers->scenario, "Setting accept encoding to all types.");
 	// Включение приема всех допустимых кодировок данных.
 	curl_easy_setopt(hCurl, CURLOPT_ACCEPT_ENCODING, "");
 
-	zlog_info(zlog_programm_category, "Setting write function.");
+	zlog_info(loggers->scenario, "Setting write function.");
 	curl_easy_setopt(hCurl, CURLOPT_WRITEFUNCTION, save_response_data);
 
-	zlog_info(zlog_programm_category, "Setting write data pointer.");
+	zlog_info(loggers->scenario, "Setting write data pointer.");
 	curl_easy_setopt(hCurl, CURLOPT_WRITEDATA, response_data);	
 
 	// Задание заголовков запроса.
 	if(headers != NULL){
-		zlog_info(zlog_programm_category, "Setting headers.");
+		zlog_info(loggers->scenario, "Setting headers.");
 		curl_easy_setopt(hCurl, CURLOPT_HTTPHEADER, headers);
 	}
 	else{
-		zlog_info(zlog_programm_category, "Headers parameter is empty. Using default user-agent: \"%s\".", user_agent);
+		zlog_info(loggers->scenario, "Headers parameter is empty. Using default user-agent: \"%s\".", user_agent);
 		curl_easy_setopt(hCurl, CURLOPT_USERAGENT, user_agent);
 	}
 	time_t * time_val = (time_t*)malloc(sizeof(time_t));
 	if(time_val == NULL){
-		zlog_error(zlog_programm_category, "Failed to allocate memory for time.");
+		zlog_error(loggers->scenario, "Failed to allocate memory for time.");
 	}
 	else{
 		time(time_val);
 	}
-	zlog_info(zlog_programm_category, "Setting private request data.");
+	zlog_info(loggers->scenario, "Setting private request data.");
 	curl_easy_setopt(hCurl, CURLOPT_PRIVATE, time_val);
 
-	zlog_info(zlog_programm_category, "Performing cUrl request.");
+	zlog_info(loggers->scenario, "Performing cUrl request.");
 	// Perform request.
 	CURLcode result = curl_easy_perform(hCurl);
 	
@@ -211,36 +219,36 @@ static long http_request(char * name, char * url, struct curl_slist * headers, r
  */
 static size_t save_response_data(void* buffer, size_t size, size_t nmemb, void * userp){
 	size_t result = size * nmemb;
-	zlog_debug(zlog_programm_category, "size * nmemb = %u.", result);
+	zlog_debug(loggers->scenario, "size * nmemb = %u.", result);
 	if(userp == NULL){
-		zlog_debug(zlog_programm_category, "User data pointer is NULL. Nothing to do.");
+		zlog_debug(loggers->scenario, "User data pointer is NULL. Nothing to do.");
 		return result;
 	}
 	if(result == 0){
-		zlog_debug(zlog_programm_category, "Result = 0. Nothing to do");
+		zlog_debug(loggers->scenario, "Result = 0. Nothing to do");
 		return 0;
 	}
 	size_t current_size = ((response_data_struct*)userp)->size;
 	
 	void ** response = &(((response_data_struct*)userp)->data);
 	if(*response == NULL){
-		zlog_info(zlog_programm_category, "Allocating memory for userp->data.");
+		zlog_info(loggers->scenario, "Allocating memory for userp->data.");
 		*response = malloc(result + current_size);
 	}
 	else{
-		zlog_info(zlog_programm_category, "Reallocating memory for userp->data.");
+		zlog_info(loggers->scenario, "Reallocating memory for userp->data.");
 		*response = realloc(*response, result + current_size);	
 	}
 	if(response == NULL){
-		zlog_error(zlog_programm_category, "Memory allocation/reallocation failed.");
+		zlog_error(loggers->scenario, "Memory allocation/reallocation failed.");
 		return 0;
 	}
 	void * response_last_byte = *response + current_size - 1;
-	zlog_debug(zlog_programm_category, "Response pointer value: %p.\tresponse_last_byte pointer value: %p.", response, response_last_byte);
-	zlog_info(zlog_programm_category, "Copiyng from [buffer] to [userp->data].");
+	zlog_debug(loggers->scenario, "Response pointer value: %p.\tresponse_last_byte pointer value: %p.", response, response_last_byte);
+	zlog_info(loggers->scenario, "Copiyng from [buffer] to [userp->data].");
 	memcpy(response_last_byte, buffer, result);
 	if(*response == NULL){
-		zlog_error(zlog_programm_category, "Failed to copy from [buffer] to [userp->data].");
+		zlog_error(loggers->scenario, "Failed to copy from [buffer] to [userp->data].");
 		return 0;
 	}
 	((response_data_struct*)userp)->size += result;
--- creator/transactions.c
+++ creator/transactions.c
@@ -1,26 +1,27 @@
 #include "transactions.h"
-#include "logger.h"
-#include <time.h>
+#include <logger.h>
+#include <windows.h>
 
-static void save_statistics(transaction * transaction);
+static void save_statistics(transaction *);
+static long long get_duration(transaction *);
+static long long get_ticks_count();
 extern zlog_categories * loggers;
 
 transaction transaction_begin(char * name){
-    // Получить текущее значение времени. Передать полученное значение и название транзакции в хранилище
-    
-    return (transaction){
-        .start = time(NULL),
-        .name = name
+    // Получить текущее значение времени. Передать полученное значение и название транзакции в хранилище    
+    transaction tr = {        
+        .name = name,
+        .start_ticks_count = get_ticks_count()
     };
+    return tr;
 }
 
 void transaction_end(transaction * transaction, transaction_status status){
-    time_t end = clock();
     if(transaction == NULL){
         zlog_error(loggers->common, "Transaction is NULL");
         return;
     }
-    transaction->end = time(NULL);
+    transaction->end_ticks_count = get_ticks_count();
     transaction->status = status;
     save_statistics(transaction);
 }
@@ -31,11 +32,33 @@ static void save_statistics(transaction * transaction){
         return;
     }
     char * status = transaction->status ? "SUCCESS" : "FAIL";
+    long long duration = get_duration(transaction);
     zlog_debug(loggers->common
-                , "Start time: %li. End time: %li. Name: %s. Status: %s"
-                , transaction->start, transaction->end, transaction->name, status);
-    char * format_string = "%li,%s,%li,%s";
-    long duration = transaction->end - transaction->start;
-    zlog_info(loggers->statistics, format_string, transaction->start, transaction->name, duration, status);
+                , "Start time: %lli. Duration: %lli. Name: %s. Status: %s"
+                , transaction->start_time_ms, duration, transaction->name, status);
+    char * format_string = "%li,%s,%lli,%s";
+    zlog_info(loggers->statistics, format_string, transaction->start_time_ms, transaction->name, duration, status);
     return;
+}
+
+/**
+ * Returns the operation duration in milliseconds
+ * 
+ */
+static long long get_duration(transaction * trnsctn){
+    static LARGE_INTEGER frequency; 
+    if(frequency.QuadPart == 0){
+        QueryPerformanceFrequency(&frequency);
+    }
+    long long difference = trnsctn->end_ticks_count - trnsctn->start_ticks_count;
+    //To eliminate loss of precision convert result to microseconds.
+    difference *= 1000000;
+    difference /=frequency.QuadPart;
+    return difference;
+}
+
+static long long get_ticks_count(){
+    LARGE_INTEGER time;
+    QueryPerformanceCounter(&time);
+    return time.QuadPart;
 }
